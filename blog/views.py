f842973f692092e234c63a4d10e8938f3e20279d
DevJebez <jebezoswald.2965@gmail.com>
2025-06-20 19:56:22 +0530

    Added home page and about page

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,25 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here
-
+posts = [ 
+    {
+        'author':'Jebez Oswald',
+        'title':'Blog post 1',
+        'content':'First post content',
+        'date_posted':'6 June 2025'
+    },
+    {
+        'author' : 'Jethro',
+        'title':'Blog post 2',
+        'content':'Second post contet',
+        'date_posted':'7 June 2025'
+    }
+]
 def home(request):
-    return HttpResponse('<h1>Home page</h1>')
+    context = {
+        'posts':posts
+    }
+    return render(HttpResponse,'blog/home.html',context)
+
+def about(request):
+    return render(HttpResponse,'blog/about.html')
